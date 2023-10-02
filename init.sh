7662e646f27dad30c8a7a00a4b6dbd660629bcf4
deokgyuhan <103297519+deokgyuhan@users.noreply.github.com>
2023-10-02 23:23:20 +0900

    2023.10.02 commit(2)

---
+++ init.sh
@@ -0,0 +1,46 @@
+# Define the versions to download
+opencv_version="4.3.0"
+
+# Define the paths to the directories where the files will be installed
+projectPath="../"
+opencvIOSPath="$projectPath/ios"
+opencvIncludePath="$projectPath/src/"
+opencvJNIPath="$projectPath/android/src/main/jniLibs/"
+
+# Create the download directory
+mkdir -p download
+cd download
+
+# Download the opencv source code and unzip it
+wget -O "opencv-$opencv_version-android-sdk.zip" "https://github.com/opencv/opencv/releases/download/$opencv_version/opencv-$opencv_version-android-sdk.zip"
+wget -O "opencv-$opencv_version-ios-framework.zip" "https://github.com/opencv/opencv/releases/download/$opencv_version/opencv-$opencv_version-ios-framework.zip"
+unzip "opencv-$opencv_version-android-sdk.zip"
+unzip "opencv-$opencv_version-ios-framework.zip"
+
+# remove opencv from ios project
+rm -R "$opencvIOSPath/opencv2.framework"
+
+# remove opencv from android project
+rm -R "$opencvIncludePath/include"
+rm -R "$opencvJNIPath"
+
+# copy opencv to ios project
+cp -R opencv2.framework "$opencvIOSPath"
+
+# print success message for ios
+echo "OpenCV $opencv_version for iOS has been successfully installed"
+
+# copy opencv to android project
+cp -R OpenCV-android-sdk/sdk/native/jni/include "$opencvIncludePath"
+mkdir -p "$opencvJNIPath"
+cp -R OpenCV-android-sdk/sdk/native/libs/* "$opencvJNIPath"
+
+# print success message for android
+echo "OpenCV $opencv_version for Android has been successfully installed"
+
+# run the update_ios.sh script to copy the source files to the ios project
+cd ..
+sh update_ios.sh
+
+# remove the downloaded files
+rm -R download
---
+++ update_ios.sh
@@ -0,0 +1,15 @@
+# This script copies the source files from the src directory to the ios directory.
+# Should be run every time the src directory files are updated.
+
+srcPath="src"
+iosSrcPath="ios/Classes/src"
+
+# Remove the source files if they exist
+rm -rf $iosSrcPath
+
+# create the source directories
+mkdir -p $iosSrcPath
+
+# Copy the source files
+rsync -av --exclude '*.txt' --exclude "include/" "$srcPath/" "$iosSrcPath/"
+
